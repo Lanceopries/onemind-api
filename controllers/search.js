54f676fa77b5b0081312785b7db8e3781b367941
Lanceopries <35199929+Lanceopries@users.noreply.github.com>
2021-06-19 11:02:37 +0300

    fix(search): small build fix

--- controllers/search.js
+++ controllers/search.js
@@ -2,6 +2,7 @@ const PersonProcessService = require("../services/process.person.service");
 const Validators = require("../utils/validators");
 const ClientSearchData = require("../models/Entities/service.data");
 const { json } = require("body-parser");
+const errorHandler = require("../utils/errorHandler");
 
 module.exports.search = async function (req, res) {
   if (!req.body.data) {
@@ -10,6 +11,7 @@ module.exports.search = async function (req, res) {
     });
   }
   let data = req.body.data;
+  
   let persons = [];
   for (let param of data) {
     if (await Validators.isInnValid(param)) {
@@ -21,8 +23,9 @@ module.exports.search = async function (req, res) {
   }
 
   try {
+    let info = [];
     for(let person of persons){
-       let info = await PersonProcessService.processPerson(person)
+       info.push(await PersonProcessService.processPerson(person))
     }
     res.status(201).json(info);
   } catch (e) {
